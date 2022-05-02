e837063a29c67464be09a9f39e772347770287b7
Swetha0707 <103947484+Swetha0707@users.noreply.github.com>
2022-05-02 16:29:41 +0530

    image and anchor tag binding

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
-const app=Vue.createApp(
+const app = Vue.createApp(
     {
-        data(){
+        data() {
             return {
-                showBooks:true,
-                title:'The final Empire',
-                author:'Brandom samuel',
-                age:'45',
-                x:0,
-                y:0
+                url:'https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Vue_getting_started',
+                showBooks: true,
+                books: [
+                    {
+                        title: 'name of the wind',
+                        author: 'patrick johnson',
+                        img:'assets/1.jpg'
+                    },
+                    {
+                        title: 'World of Power',
+                        author: 'John with',
+                        img:'assets/2.jpg'
+
+                    },
+                    {
+                        title: 'narnia',
+                        author: 'Fedrick Bason',
+                        img:'assets/3.jpg'
+
+                    }
+
+                ]
             }
         },
-        methods:{
-           toggleShowBooks(){
-               this.showBooks=!this.showBooks;
-           },
-           handleEvent(e){
-               console.log(e, e.type)
-               if(data){
-                   console.log(data)
-               }
-           },
-           handleMousemove(e){
-                this.x=e.offsetX
-                this.y=e.offsetY
-           }
+        methods: {
+            toggleShowBooks() {
+                this.showBooks = !this.showBooks;
+            },
+            handleEvent(e) {
+                console.log(e, e.type)
+                if (data) {
+                    console.log(data)
+                }
+            },
+            handleMousemove(e) {
+                this.x = e.offsetX
+                this.y = e.offsetY
+            }
         }
     }
 )

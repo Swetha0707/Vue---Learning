99055bcfd1c2dc0844ee2bf376798a9e5447f067
Swetha0707 <103947484+Swetha0707@users.noreply.github.com>
2022-05-02 16:51:19 +0530

    Toggle Event

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ const app = Vue.createApp(
     {
         data() {
             return {
-                url:'https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Vue_getting_started',
+                url: 'https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Vue_getting_started',
                 showBooks: true,
                 books: [
                     {
                         title: 'name of the wind',
                         author: 'patrick johnson',
-                        img:'assets/1.jpg'
+                        img: 'assets/1.jpg',
+                        isFav: true
                     },
                     {
                         title: 'World of Power',
                         author: 'John with',
-                        img:'assets/2.jpg'
+                        img: 'assets/2.jpg',
+                        isFav: false
 
                     },
                     {
                         title: 'narnia',
                         author: 'Fedrick Bason',
-                        img:'assets/3.jpg'
+                        img: 'assets/3.jpg',
+                        isFav: true
 
                     }
 
@@ -28,7 +31,11 @@ const app = Vue.createApp(
         },
         methods: {
             toggleShowBooks() {
-                this.showBooks = !this.showBooks;
+                this.showBooks = !this.showBooks
+            },
+            toggleEvent(book) {
+                book.isFav = !book.isFav
+
             },
             handleEvent(e) {
                 console.log(e, e.type)
